a219be49bbafbd2de4cd9dfeb038237d7fd6d865
Lucas Rodrigues <lucas.bartllb@gmail.com>
2025-10-23 18:37:30 -0300

    front qrcode

---
+++ scripts.js
@@ -0,0 +1,31 @@
+const content = document.querySelector("#content");
+const btn = document.querySelector("#btn");
+
+
+function criacaoQR(){
+
+     let QRCode = [];
+    
+     const generateCode = new QRCode (document.getElementById("qrcode"),{
+
+
+
+        
+     })
+
+
+
+
+}
+
+
+btn.addEventListener("click",(event)=>{
+
+    event.preventDefault();  
+
+    
+
+
+
+
+})
