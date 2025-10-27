894112f530217245044eb18760f14d74744a2104
Lucas Rodrigues <lucas.bartllb@gmail.com>
2025-10-27 14:27:34 -0300

    finalizaçao da geracao

--- scripts.js
+++ scripts.js
@@ -1,31 +1,47 @@
 const content = document.querySelector("#content");
 const btn = document.querySelector("#btn");
+const primary = document.querySelector("#primary");
+const secondary = document.querySelector("#secondary");
+const qrcode = document.querySelector("#qrcode");
+ 
+content.oninput  = ()=>{
 
-
-function criacaoQR(){
-
-     let QRCode = [];
+ 
     
-     const generateCode = new QRCode (document.getElementById("qrcode"),{
+}
 
 
+function createQR(){
 
-        
+     const generateCode = new QRCode (document.querySelector("#qrcode"),{
+
+        text: content.value,
+        with: 250,
+        height: 250
+
+
      })
-
-
-
 
 }
 
 
 btn.addEventListener("click",(event)=>{
-
+    
     event.preventDefault();  
 
+    if(content.value === ""){
+       return alert("Campo deve estar preenchido!");
+        
+    }else{
+        primary.classList.add("transparent");  
+        secondary.classList.remove("transparent");
+        qrcode.classList.add("qr");
+        createQR()
+    }
+    
+    
     
 
 
-
-
 })
+
